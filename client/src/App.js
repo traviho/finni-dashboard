76c807d44f90cdbde066b4208b6ea77e5be4e2b2
Travis Ho <traviho@traviss-mb-pro.myfiosgateway.com>
2023-11-24 22:13:28 -0500

    name filter

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import logo from './logo_orange.svg'
 
 function App() {
   const [data, setData] = useState([])
+  const [nameSearch, setNameSearch] = useState("")
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,27 +17,43 @@ function App() {
     fetchData();
   }, [])
 
+  const getTable = () => {
+    return (
+      <div className='patients_table_container'>
+        <table className='patients_table'>
+          <thead>
+            <tr>
+              <th class="th_name">Name</th>
+              <th class="th_birthday">Birthday</th>
+              <th class="th_status">Status</th>
+              <th class="th_address">Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.filter(obj => obj["name"].toUpperCase().includes(nameSearch.toUpperCase())).map(obj => (
+              <tr>
+                <td>{obj["name"]}</td>
+                <td>{obj["birthday"]}</td>
+                <td>{obj["status"]}</td>
+                <td>{obj["address"]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
   return (
     <div className='container'>
       <div className='navbar'>
         <img className='logo' src={logo} />
       </div>
-      <table className='patients_table'>
-        <tr>
-          <th>Name</th>
-          <th>Birthday</th>
-          <th>Status</th>
-          <th>Address</th>
-        </tr>
-        {data.map(obj => (
-          <tr>
-            <td>{obj["name"]}</td>
-            <td>{obj["birthday"]}</td>
-            <td>{obj["status"]}</td>
-            <td>{obj["address"]}</td>
-          </tr>
-        ))}
-      </table>
+      <div className='name-search'>
+        <label>Find Patient:  </label>
+        <input type="text" placeholder='name' onChange={event => setNameSearch(event.target.value)} />
+      </div>
+      {getTable()}
     </div>
   )
 }
